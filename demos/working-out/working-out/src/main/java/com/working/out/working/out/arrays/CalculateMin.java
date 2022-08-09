82d28b5d7cca7dea200ecfeaee4d56ad24dda491
fernando <lfmartinezpantoja@gmail.com>
2022-08-09 08:32:32 -0500

    arrays exercices

---
+++ demos/working-out/working-out/src/main/java/com/working/out/working/out/arrays/CalculateMin.java
@@ -0,0 +1,18 @@
+package com.working.out.working.out.arrays;
+
+/**
+ * Escribir un programa que tome el array de nombre sueldos (esta variable son
+ * los salarios de los empleados de una empresa) y encuentre el salario mas bajo
+ */
+public class CalculateMin {
+
+    public static void main(String[] args) {
+
+        int[] sueldos = {1000000, 2500000, 3000000, 4000000, 10000000, 1500000, 1500000, 3000000, 4000000, 11000000};
+
+        /**
+         * encontrar el  salario mas bajo e imprimirlo por pantalla
+         */
+
+    }
+}
---
+++ demos/working-out/working-out/src/main/java/com/working/out/working/out/arrays/CalculateTheAverage.java
@@ -0,0 +1,18 @@
+package com.working.out.working.out.arrays;
+
+/**
+ * Escribir un programa que tome el array de nombre sueldos (esta variable son
+ * los salarios de los empleados de una empresa) y calcule el promedio
+ */
+public class CalculateTheAverage {
+
+    public static void main(String[] args) {
+
+        int[] sueldos = {1000000, 2500000, 3000000, 4000000, 10000000, 1500000, 1500000, 3000000, 4000000, 10000000};
+
+        /**
+         * Calcular el promedio e imprimirlo por pantalla
+         */
+
+    }
+}
